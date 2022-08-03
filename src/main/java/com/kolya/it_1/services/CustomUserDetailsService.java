8360365b960494ac0027e68c0c5da395a5810a84
pentonzxc <nikolai.urusov@gmail.com>
2022-08-03 18:53:42 +0300

    pom.xml fixed

--- src/main/java/com/kolya/it_1/services/CustomUserDetailsService.java
+++ src/main/java/com/kolya/it_1/services/CustomUserDetailsService.java
@@ -22,7 +22,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return detailsMapper.convert(userService.findUserByEmail(email)
+            return detailsMapper.convert(userService.findUserByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException(
                         String.format("User with email %s doesn't exist", email))));
     }
