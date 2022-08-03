bbfefa72730a3dc7dfbcf247cbca483e254c8759
pentonzxc <nikolai.urusov@gmail.com>
2022-08-03 12:53:56 +0300

    final version

--- src/main/java/com/kolya/it_1/controllers/UserController.java
+++ src/main/java/com/kolya/it_1/controllers/UserController.java
@@ -7,10 +7,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
     public final UserService userService;
 
--- src/main/java/com/kolya/it_1/security/SecurityConfig.java
+++ src/main/java/com/kolya/it_1/security/SecurityConfig.java
@@ -6,11 +6,12 @@ import com.kolya.it_1.services.CustomUserDetailsService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService customUserDetailsService;
 
@@ -18,7 +19,7 @@ public class SecurityConfig {
 
     private final LoginSuccessHandler loginSuccessHandler;
 
-    private final AccessFilter accessFilter;
+    private  final AccessFilter accessFilter;
 
 
     public SecurityConfig(CustomUserDetailsService customUserDetailsService, PasswordEncoder passwordEncoder, LoginSuccessHandler loginSuccessHandler, AccessFilter accessFilter) {
@@ -29,24 +30,25 @@ public class SecurityConfig {
     }
 
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf()
-                .disable()
-                .addFilterAfter(accessFilter, BasicAuthenticationFilter.class)
+                  .disable()
+                .addFilterAfter(accessFilter , BasicAuthenticationFilter.class)
                 .anonymous().authorities("anonymous")
                 .and()
                 .authorizeRequests()
-                   .antMatchers("/register").permitAll()
-                   .anyRequest()
-                   .authenticated()
+                  .antMatchers("/register").permitAll()
+                  .anyRequest()
+                  .authenticated()
                 .and()
                 .formLogin().permitAll()
-                   .loginPage("/login")
-                   .loginProcessingUrl("/perform-login")
-                   .usernameParameter("email")
-                   .passwordParameter("password")
-                   .successHandler(loginSuccessHandler)
+                  .loginPage("/login")
+                  .loginProcessingUrl("/perform-login")
+                  .usernameParameter("email")
+                  .passwordParameter("password")
+                  .successHandler(loginSuccessHandler)
                 .and()
                 .logout().permitAll()
                   .deleteCookies("JSESSIONID")
@@ -55,6 +57,7 @@ public class SecurityConfig {
     }
 
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
     }
